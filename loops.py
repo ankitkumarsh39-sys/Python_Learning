988629c124dfd26530bdca11a7eb3ab81ea3c817
Ankit.Kumar <ankitkumarsh39@gmail.com>
2026-02-16 13:27:10 +0530

    commit

--- loops.py
+++ loops.py
@@ -2,5 +2,16 @@
 # for loop is used to iterate over a sequence (like a list, tuple, string)
 # while loop is used to execute a block of code as long as a condition is true
 # nested loop is a loop inside another loop
+# iterate means to repeat a process or a set of instructions. In programming, it often refers to the process of looping through a collection of items, such as a list or an array, and performing some operation on each item. This can be done using various types of loops, such as for loops or while loops, depending on the specific requirements of the task at hand.
 for i in range(5):
     print(i)
+
+count = 1
+while count <= 5:
+    print("hello", count)
+    count += 1
+# print numbers from 1 to 10 using for loop
+x =1
+while x <= 10:
+    print("NUMBER", x)
+    x += 1
