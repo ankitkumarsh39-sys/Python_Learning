a8a98c27d4c7c5346aa96c309d9c969f5c051544
Ankit.Kumar <ankitkumarsh39@gmail.com>
2026-02-16 12:52:40 +0530

    loops commit

---
+++ loops.py
@@ -0,0 +1,6 @@
+# loops is a collection of functions that demonstrate various types of loops in Python.
+# for loop is used to iterate over a sequence (like a list, tuple, string)
+# while loop is used to execute a block of code as long as a condition is true
+# nested loop is a loop inside another loop
+for i in range(5):
+    print(i)
